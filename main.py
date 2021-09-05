8e8c25b9b4fb522c410c9f34f9a1d6ac966a3bd8
Zheng Huang <hz1624917200@outlook.com>
2021-09-05 17:16:20 +0800

    Main Skeleton build

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Default page, will add main script
 import pyfiglet
+from sys import argv
+import Network
 
 
 def print_hi(name):
@@ -24,11 +26,50 @@ def print_hi(name):
 	pyfiglet.print_figlet('Kuwo-Spider', font=random.choice(my_font),
 	colors=random.choice(list(pyfiglet.COLOR_CODES.keys())), width=100)
 	# Use a breakpoint in the code line below to debug your script.
-	print(f'By {name}.')  # Press Ctrl+F8 to toggle the breakpoint.
+	print(f'By {name}.\n')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def menu() -> str:
+	print("Type to search something: (type 'quit()' to quit and 'rank()' to view ranking board)")
+	print('tips: input nothing to hang around\n')
+	return input()
+
+
+def hanging_around():
+	print('Constructing...')
+	pass
+
+
+def rank():
+	print('Constructing...')
+	pass
+
+
+def search(kw: str) -> None:
+	print("Searching...")
+	pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-	print_hi('ZhengHuang and HeQi')
 	# print a banner
+	print_hi('ZhengHuang and HeQi')
 
+	if len(argv) > 1:
+		...
+		exit()
+
+	while True:
+		keyword = menu()
+
+		# special keywords
+		if keyword == 'quit()':
+			print('Bye.')
+			exit()
+		elif keyword == '':
+			hanging_around()
+		elif keyword == 'rank()':
+			rank()
+		else:
+			# normal keyword search
+			search(keyword)
